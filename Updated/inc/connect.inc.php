499234905eba4aa060eed721e3ac4fce068579e5
NguyenLy04 <74318064+NguyenLy04@users.noreply.github.com>
2020-11-14 00:40:44 +0700

    Update connect.inc.php

--- Updated/inc/connect.inc.php
+++ Updated/inc/connect.inc.php
@@ -1,4 +1,4 @@
 <?php 
-	mysql_connect("localhost","root","") or die("Couldn't connect to SQL server");
+	mysql_connect("database-1.cgc4rlsrtywf.us-east-2.rds.amazonaws.com","admin","Lynguyen") or die("Couldn't connect to SQL server");
 	mysql_select_db("grocerydb") or die("Couldn'ttt select DB");
 ?>

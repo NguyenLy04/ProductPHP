7822163dd4c9bdc0301a074128f1bae78e9121b4
NguyenLy04 <74318064+NguyenLy04@users.noreply.github.com>
2020-11-14 00:20:27 +0700

    Update connect.inc.php

--- E-Commerce Full Website Using PHP/inc/connect.inc.php
+++ E-Commerce Full Website Using PHP/inc/connect.inc.php
@@ -1,4 +1,4 @@
 <?php 
-	mysql_connect("localhost","root","") or die("Couldn't connet to SQL server");
+	mysql_connect("database-1.cgc4rlsrtywf.us-east-2.rds.amazonaws.comt","admin","Lynguyen") or die("Couldn't connet to SQL server");
 	mysql_select_db("ebuybd") or die("Couldn'ttt select DB");
 ?>
